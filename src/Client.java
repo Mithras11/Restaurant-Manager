fca7b204a2c33670ff055e7fd6cd8de0ec3f34c6
kaloyan.ivanov <kaloyan.ivanov88@gmail.com>
2022-05-05 11:09:41 +0300

    first query added

--- src/Client.java
+++ src/Client.java
@@ -1,10 +1,17 @@
 import utils.*;
 
+import java.io.Console;
+
 public class Client {
     public static void main(String[] args) {
         //populate products list
         var foodProducts = Administrator.populateFoodProducts();
         var ordersHistory = Playwright.createRandomOrderHistory(foodProducts);
         var dataProcessor = new DataProcessor(foodProducts, ordersHistory);
+
+//------------------------------------------------------------------------------------------
+//queries
+//1. Food products list - select all enabled products
+        System.out.println("1. Food products list - select all enabled products");
     }
 }
